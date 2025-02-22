6ef79088fc41dafe749e74a011db3575900d9cfa
tilupe <timothy@pescatore.ch>
2025-02-22 12:47:12 +0100

    use smart split pluign

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -482,22 +482,32 @@ return {
     end,
   },
   {
-    'alexghergh/nvim-tmux-navigation',
+    'mrjones2014/smart-splits.nvim',
     config = function()
-      local nvim_tmux_nav = require 'nvim-tmux-navigation'
-
-      nvim_tmux_nav.setup {
-        disable_when_zoomed = true, -- defaults to false
-      }
-
-      vim.keymap.set('n', '<C-h>', nvim_tmux_nav.NvimTmuxNavigateLeft)
-      vim.keymap.set('n', '<C-j>', nvim_tmux_nav.NvimTmuxNavigateDown)
-      vim.keymap.set('n', '<C-k>', nvim_tmux_nav.NvimTmuxNavigateUp)
-      vim.keymap.set('n', '<C-l>', nvim_tmux_nav.NvimTmuxNavigateRight)
-      vim.keymap.set('n', '<C-\\>', nvim_tmux_nav.NvimTmuxNavigateLastActive)
-      vim.keymap.set('n', '<C-Space>', nvim_tmux_nav.NvimTmuxNavigateNext)
+      vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
+      vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
+      vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
+      vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
+      vim.keymap.set('n', '<C-\\>', require('smart-splits').move_cursor_previous)
     end,
   },
+  -- {
+  --   'alexghergh/nvim-tmux-navigation',
+  --   config = function()
+  --     local nvim_tmux_nav = require 'nvim-tmux-navigation'
+  --
+  --     nvim_tmux_nav.setup {
+  --       disable_when_zoomed = true, -- defaults to false
+  --     }
+  --
+  --     vim.keymap.set('n', '<C-h>', nvim_tmux_nav.NvimTmuxNavigateLeft)
+  --     vim.keymap.set('n', '<C-j>', nvim_tmux_nav.NvimTmuxNavigateDown)
+  --     vim.keymap.set('n', '<C-k>', nvim_tmux_nav.NvimTmuxNavigateUp)
+  --     vim.keymap.set('n', '<C-l>', nvim_tmux_nav.NvimTmuxNavigateRight)
+  --     vim.keymap.set('n', '<C-\\>', nvim_tmux_nav.NvimTmuxNavigateLastActive)
+  --     vim.keymap.set('n', '<C-Space>', nvim_tmux_nav.NvimTmuxNavigateNext)
+  --   end,
+  -- },
   -- {
   --   'christoomey/vim-tmux-navigator',
   --   cmd = {
