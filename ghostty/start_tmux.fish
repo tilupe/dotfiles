b8b596f442bb397a3292361966031f9f263d85a1
tilupe <timothy@pescatore.ch>
2025-02-24 13:18:41 +0100

    always attach to tmux when starting ghostty

---
+++ ghostty/start_tmux.fish
@@ -0,0 +1,15 @@
+#!/usr/bin/env fish
+
+set SESSION_NAME "ghostty"
+
+# Check if the session already exists
+if tmux has-session -t $SESSION_NAME 2>/dev/null
+    # If the session exists, reattach to it
+    tmux attach-session -t $SESSION_NAME
+else
+    # If the session doesn't exist, start a new one
+    tmux new-session -s $SESSION_NAME -d
+    # Run fastfetch in the newly created tmux session
+    tmux send-keys -t $SESSION_NAME 'fastfetch' C-m
+    tmux attach-session -t $SESSION_NAME
+end
