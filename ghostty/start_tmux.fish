75a53f7b64970cad00cf1a583205a01c4014fc52
tilupe <timothy@pescatore.ch>
2025-05-08 16:28:26 +0200

    back to start

--- ghostty/start_tmux.fish
+++ ghostty/start_tmux.fish
@@ -4,7 +4,8 @@ set SESSION_NAME "ghostty"
 
 # Check if the session already exists
 if tmux has-session -t $SESSION_NAME 2>/dev/null
-  ghostty -e "Session $SESSION_NAME already exists."
+    # If the session exists, reattach to it
+    tmux attach-session -t $SESSION_NAME
 else
     # If the session doesn't exist, start a new one
     tmux new-session -s $SESSION_NAME -d
